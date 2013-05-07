4d74215d442ce227a1c5feb23212d8442ed5eca7
Hagen Paul Pfeifer <hagen@jauu.net>
2013-05-08 00:20:23 +0200

    device name as argument
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- prio-fq-codel.sh
+++ prio-fq-codel.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-eth=eth0
-tc qdisc del dev $eth root 2>/dev/null
+if [ $# -eq 0 ]
+then
+	echo "Usage $0 <dev>"
+	exit
+fi
+
+tc qdisc del dev $1 root 2>/dev/null
 tc -batch << EOF
-qdisc add dev $eth root handle 1: prio bands 3
-qdisc add dev $eth parent 1:1 handle 11: fq_codel
-qdisc add dev $eth parent 1:2 handle 12: fq_codel
-qdisc add dev $eth parent 1:3 handle 13: fq_codel
+qdisc add dev $1 root handle 1: prio bands 3
+qdisc add dev $1 parent 1:1 handle 11: fq_codel
+qdisc add dev $1 parent 1:2 handle 12: fq_codel
+qdisc add dev $1 parent 1:3 handle 13: fq_codel
 EOF
